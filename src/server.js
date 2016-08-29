14fbaaeb9330dc023bc05f3233f2ca43d9564180
Matt Hackmann <matt@dxprog.com>
2016-08-28 20:56:54 -0700

    added json parsing for express

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const config = require('../config');
 
 const app = express();
+app.use(bodyParser.json());
 
-// Bootstrap the database connection
+// Bootstrap the database and memcached connections
 require('./db');
+require('./cache');
 
 // Load up the routes
 [
