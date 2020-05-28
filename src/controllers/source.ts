c51ec2c757daba891f24c611e94a723f5e89495f
Matt Hackmann <matt@dxprog.com>
2020-05-27 21:47:17 -0700

    get single source by id endpoint

--- src/controllers/source.ts
+++ src/controllers/source.ts
@@ -4,7 +4,7 @@ import { Controller } from '../lib/controller';
 import { RouteMap } from '../interfaces/route';
 import { MysqlDb } from '../lib/mysql-db';
 import { SourceModel } from '../models/source';
-import { cache } from '../lib/cache';
+import { cache, CacheDuration } from '../lib/cache';
 
 export class SourceController extends Controller {
   public static create(db: MysqlDb): SourceController {
@@ -16,9 +16,17 @@ export class SourceController extends Controller {
     return await SourceModel.selectAll(this.db);
   }
 
+  @cache('sourceById', CacheDuration.LONG, [ 'sourceId' ])
+  public async getSourceById({ sourceId }: { sourceId: number }): Promise<any> {
+    return await SourceModel.selectById(this.db, sourceId);
+  }
+
   public getRouteMap(): RouteMap {
     return {
-      'get:/sources': this.getSources.bind(this)
+      'get:/sources': this.getSources.bind(this),
+      'get:/sources/:sourceId': async (req: express.Request) => (
+        this.getSourceById({ sourceId: parseInt(req.params.sourceId, 10) })
+      ),
     };
   }
 }
